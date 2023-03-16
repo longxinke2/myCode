be1ae7057af05f7c5dad625ced8dd141b56ce701
**** <*******@163.com>
2023-03-16 16:17:57 +0800

    add my code

---
+++ lxk.py
@@ -0,0 +1,21 @@
+import requests
+
+def translate():
+    s = input(":")
+    dat = {
+        "kw":s
+    }
+    resp = requests.post("https://fanyi.baidu.com/sug", data=dat)
+    print(resp.json()['data'][0]['v']) #将返回的内容直接返回为json
+
+def is_English():
+    string = input(":")
+    for ch in string:
+        if ord(ch) >= 128:
+            return ch
+    return True
+
+def progress_bar(now,end):
+    percent = now*100//end
+    line = '-'*(percent//2)+' '*(50-percent//2)
+    print(f'\r{line}{percent}%',end="")
